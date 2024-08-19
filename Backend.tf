2b23d2e9f903c73a2383d79c5cc570c1a34806e7
Karim Warda <109697567+Km-Warda@users.noreply.github.com>
2024-08-19 08:25:32 +0300

    Update Backend.tf

--- Backend.tf
+++ Backend.tf
@@ -1,8 +1,22 @@
+# Stores the statefile remotely 
 terraform {
   backend "s3" {
     bucket         = "terraform-state-bucket-karim"
     key            = "terraform.tfstate"
-    region         = var.aws_region
+    region         = "us-east-1"
     encrypt        = true
+  }
+}
+
+
+# Creates a read-only copy of the statefile
+data "terraform_remote_state" "read-only-state-file" {
+  backend = "s3"
+  config {
+    bucket     = "terraform-state-bucket-karim"
+    key        = "terraform.tfstate"
+    access_key = "${var.AWS_ACCESS_KEY}"
+    secret_key = "${var.AWS_SECRET_KEY}"
+    region     = ""us-east-1""
   }
 }
